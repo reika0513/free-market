37596b0a30db6cc186539819dbdf43cfcfb7550e
reika <r.okinu.513@gmail.com>
2025-03-19 07:00:03 +0900

    views commit

---
+++ src/resources/views/detail.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/detail.css') }}">
+@endsection
+
+@section('content')
+<div class="detail">
+    <div class="detail_image"></div>
+    <div class="detail_items"></div>
+</div>
+@endsection
